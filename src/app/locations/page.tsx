f212517e7ce879e8d2f2575751fe7187ff73dca0
Josh W <jwyrick7@gmail.com>
2025-08-25 22:45:26 +0000

    oops i wanted that last image here can you move it

--- src/app/locations/page.tsx
+++ src/app/locations/page.tsx
@@ -23,7 +23,7 @@ export default function LocationsPage() {
         <section className="relative h-[400px] overflow-hidden bg-background flex items-center justify-center text-center text-foreground">
             <div className="absolute inset-0 w-full h-full">
             <Image
-                src="/diesel-repair-service-locations-san-luis-obispo-santa-barbara-county.png"
+                src="/diesel-repair-service-locations-san-luis-obispo-santa-barbara-county.jpg"
                 alt="Diesel service truck on Highway 101 on the Central Coast"
                 className="w-full h-full object-cover"
                 fill
